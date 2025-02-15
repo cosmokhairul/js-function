6d426f2be5b5c5c63592ef3d47279b951885c51e
cosmokhairul <porosh.means.touch@gmail.com>
2025-02-15 22:12:19 +0600

    second example

--- function.js
+++ function.js
@@ -1,8 +1,14 @@
 const price = 45;
 
 function shutTheFan(){
-    console.log("Stand Up")
+    // console.log("Stand Up")
 }
 
 shutTheFan();
 
+function sum(x,y){
+    const total = x+y;
+    console.log(total);
+}
+
+sum(10,5);
